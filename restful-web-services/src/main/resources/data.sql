0ee5dccf7e907d5e9fe140097d8108598f879b92
Vaishnavibeepata <beepyatavaishnavi@gmail.com>
2023-11-14 10:59:29 +0530

    commit

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into todo(id, description, done, target_date,username)
+values(1001,'Learn AWS' , false, CURRENT_DATE(), 'in28minutes');
+
+ insert into todo(id, description, done, target_date,username)
+values(1002,'Learn Deve0ps' , false, CURRENT_DATE(), 'in28minutes');
+
+
+insert into todo(id, description, done, target_date,username)
+values(1003,'get AWS Certified' , false, CURRENT_DATE(), 'in28minutes');
